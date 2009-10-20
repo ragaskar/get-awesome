cf96ba07fefb42a5d7eb697b095bcb2ef7be7886
ragaskar <ragaskar@gmail.com>
2009-10-20 09:06:50 -0700

    Added git color config. Removed developer names from pair script.

--- configure-git.sh
+++ configure-git.sh
@@ -2,4 +2,11 @@
 git config --global alias.st "status"
 git config --global alias.unadd "reset HEAD"
 git config --global alias.co "checkout"
+
 git config --global branch.autosetupmerge true
+
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+
