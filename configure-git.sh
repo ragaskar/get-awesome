171be4e1d21bbad77a98949c77eb819a5e41068e
ragaskar <ragaskar@gmail.com>
2009-10-20 14:44:46 -0700

    Push only current branch when invoking get push by default

--- configure-git.sh
+++ configure-git.sh
@@ -4,6 +4,7 @@ git config --global alias.unadd "reset HEAD"
 git config --global alias.co "checkout"
 
 git config --global branch.autosetupmerge true
+git config --global push.default current
 
 git config --global color.branch auto
 git config --global color.diff auto
